9d7822705a8349be4f052cd2e7bedd189b8407c7
gokul307-tech <gokulavasan307@gmail.com>
2026-03-21 23:45:18 +0530

    Update app.py

--- app.py
+++ app.py
@@ -77,7 +77,8 @@ def index():
             result = {"success": "No errors found!"}
 
     return render_template("index.html", result=result, code=code)
-
+        
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    port=int(os.environ.get("PORT",10000))
+    app.run(host="0.0.0.0",port=port)
